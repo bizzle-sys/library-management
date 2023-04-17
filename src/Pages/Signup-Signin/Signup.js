3bbbb2dba49599c682f3f51c6018cf2d89af0d2b
Reeamsh Maharjan <reamsmaharjan@hotmail.com>
2023-04-17 21:32:02 +1000

    minor changes

--- src/Pages/Signup-Signin/Signup.js
+++ src/Pages/Signup-Signin/Signup.js
@@ -8,6 +8,14 @@ import { toast } from "react-toastify";
 
 const Signup = () => {
   const [form, setForm] = useState({});
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    console.log(form);
+  };
   const inputs = [
     {
       label: "First Name",
@@ -49,6 +57,7 @@ const Signup = () => {
     <MainLayout>
       <Container className="mt-5">
         <Form
+          onSubmit={handleOnSubmit}
           className="border p-5 shadow-lg rounded m-auto mb-4 bg-light"
           style={{ width: "400px" }}>
           <h3 className="text-dark fw-bolder mb-3">Join Us!</h3>
@@ -61,17 +70,19 @@ const Signup = () => {
           <div className="mt-4">
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Account Type</Form.Label>
-              <Form.Select name="role">
+              <Form.Select onChange={handleOnChange} name="role">
                 <option value="">--Select user--</option>
-                <option value="">--Admin--</option>
-                <option value="">--User--</option>
+                <option value="Admin">--Admin--</option>
+                <option value="User">--User--</option>
               </Form.Select>
             </Form.Group>
             {inputs.map((item, i) => (
-              <CustomInput key={i} {...item} />
+              <CustomInput key={i} {...item} onChange={handleOnChange} />
             ))}
             <div className="d-grid">
-              <Button variant="dark">Register Now</Button>
+              <Button type="submit" variant="dark">
+                Register Now
+              </Button>
             </div>
           </div>
         </Form>
