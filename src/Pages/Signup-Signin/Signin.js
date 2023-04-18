7b3272bd9135e6c1676138b6bba92459bd4d27a0
Reeamsh Maharjan <reamsmaharjan@hotmail.com>
2023-04-18 21:35:42 +1000

    login form added

--- src/Pages/Signup-Signin/Signin.js
+++ src/Pages/Signup-Signin/Signin.js
@@ -1,8 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import { MainLayout } from "../../Components/Layout/MainLayout";
+import { CustomInput } from "../../Components/Layout/CustomInput/CustomInput";
+import { Button, Container, Form } from "react-bootstrap";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth, db } from "../../Config/Firebase-config/firebase-config";
+import { toast } from "react-toastify";
+import { doc, setDoc } from "firebase/firestore";
+import { useDispatch } from "react-redux";
+import { loginUser } from "./userAction";
 
 const Signin = () => {
-  return <MainLayout>Signin</MainLayout>;
+  const dispatch = useDispatch();
+  const [form, setForm] = useState({});
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
+
+    dispatch(loginUser(form));
+  };
+
+  const inputs = [
+    {
+      label: "Email",
+      name: "email",
+      type: "email",
+      placeholder: "John@email.com",
+      required: true,
+    },
+    {
+      label: "Password",
+      name: "password",
+      type: "password",
+      placeholder: "xxx",
+      required: true,
+    },
+  ];
+  return (
+    <MainLayout>
+      <Container className="mt-5">
+        <Form
+          onSubmit={handleOnSubmit}
+          className="border p-5 shadow-lg rounded m-auto mb-4 bg-light"
+          style={{ width: "400px" }}>
+          <h3 className="text-dark fw-bolder mb-3">Welcome Back!</h3>
+
+          <div className="mt-4">
+            {inputs.map((item, i) => (
+              <CustomInput key={i} {...item} onChange={handleOnChange} />
+            ))}
+            <div className="d-grid">
+              <Button type="submit" variant="dark">
+                Login Now
+              </Button>
+            </div>
+          </div>
+        </Form>
+      </Container>
+    </MainLayout>
+  );
 };
 
 export default Signin;
--- src/Pages/Signup-Signin/userAction.js
+++ src/Pages/Signup-Signin/userAction.js
@@ -1,12 +1,26 @@
-// import { toast } from "react-toastify";
-// import { db } from "../../Config/Firebase-config/firebase-config";
-// import { doc, setDoc } from "firebase/firestore";
+import { toast } from "react-toastify";
+import { auth, db } from "../../Config/Firebase-config/firebase-config";
+import { doc, setDoc } from "firebase/firestore";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
-// // create new user
-// const createUserAction = async (data) => {
-//   try {
-//     await setDoc(doc(db, "users", data));
-//   } catch (error) {
-//     toast.error(error.message);
-//   }
-// };
+// create new user
+export const loginUser = (data) => async (dispatch) => {
+  try {
+    const pendingUser = signInWithEmailAndPassword(
+      auth,
+      data.email,
+      data.password
+    );
+
+    toast.promise(pendingUser, {
+      pending: "Please wait...",
+    });
+    const { user } = await pendingUser;
+
+    if (user.uid) {
+      // get user info and mount into redux
+    }
+  } catch (error) {
+    toast.error(error.message);
+  }
+};
