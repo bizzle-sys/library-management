52c28eb675c1a04bc7c4e0a7cf7f0ec39d0fd6b7
Reeamsh Maharjan <reamsmaharjan@hotmail.com>
2023-04-21 00:21:55 +1000

    slice/stotre update

---
+++ src/Pages/book/BookSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  book: [],
+};
+const bookSlice = createSlice({
+  name: "book",
+  initialState,
+  reducers: {
+    setBook: (state, { payload }) => {
+      if (!state.book.length && !payload.length) return;
+      state.book = payload;
+    },
+  },
+});
+export const { setBook } = bookSlice.actions;
+export default bookSlice.reducer;
--- src/store.js
+++ src/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./Pages/Signup-Signin/userSlice";
+import bookReducer from "./Pages/book/BookSlice";
 export default configureStore({
   reducer: {
     user: userReducer,
+    book: bookReducer,
   },
 });
