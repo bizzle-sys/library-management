2fb6338016e30bf3589e1ac51f7cee10a35671d5
Reeamsh Maharjan <reamsmaharjan@hotmail.com>
2023-04-22 15:42:33 +1000

    minor changes

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { MainLayout } from "../../Components/Layout/MainLayout";
 import { Col, Container, Row } from "react-bootstrap";
 import { CustomCard } from "../../Components/custom-card/CustomCard";
@@ -9,11 +9,18 @@ import { HomeCarousel } from "../../Components/carousel/HomeCarousel";
 const Home = () => {
   const dispatch = useDispatch();
   const [display, setDisplay] = useState([]);
+
   const { book } = useSelector((state) => state.book);
   useEffect(() => {
     !display.length && dispatch(getAllBooksActions());
     setDisplay(book);
   }, [dispatch, book, display]);
+
+  // create local display state and assigning book to it initially
+
+  // handleonchnage to get typed value
+  // use filter to filter books based on typed value and override display state
+  // use display state to loop throughout
 
   return (
     <MainLayout>
@@ -23,6 +30,7 @@ const Home = () => {
         <Row>
           <Col>
             <h1>This is Our Library</h1>
+            <input type="text" />
             <div className="d-flex justify-content-between mt-5">
               <div>{display.length} Books Found!</div>
             </div>
